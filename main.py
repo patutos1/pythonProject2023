c5bae168924611ac2611b2c04d4d7a6668d4a25d
patutos1 <129093150+patutos1@users.noreply.github.com>
2023-04-05 14:52:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #Start of our programming project.
 #Interface code should go here:
+from tkinter import *
+
+window = Tk()
+window.title("NFL Matchup Tool")
+window.configure(width=800, height=500)
+window.configure(bg='white')
+
+winWidth = window.winfo_reqwidth()
+winwHeight = window.winfo_reqheight()
+posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
+posDown = int(window.winfo_screenheight() / 2 - winwHeight / 2)
+window.geometry("+{}+{}".format(posRight, posDown))
+
+window.mainloop()
