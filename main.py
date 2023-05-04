3e2711b33e16413048809eb29dd440552831d714
patutos1 <129093150+patutos1@users.noreply.github.com>
2023-05-03 22:19:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,24 +19,21 @@ a = ttk.Label(window, text="NFL Matchup Tool 2022", font=("Calibri", 60))
 a.pack()
 
 team1 = ttk.Label(window, text="Enter an NFL team:", font=("Calibri", 36))
-team1.place(relx = 0.0, rely = 0.15, anchor ='sw')
+team1.place(x=0, y=100)
 
 team2 = ttk.Label(window, text="Enter another NFL team:", font=("Calibri", 36))
-team2.place(relx = 0.467, rely = 0.15, anchor ='sw')
+team2.place(x=790, y=100)
 
 T1 = Text(window, height=3, width=42)
-T1.place(relx=0.22, rely=0.15, anchor='sw')
+T1.place(x=380, y=100)
 T2 = Text(window, height=3, width=42)
-T2.place(relx=0.749, rely=0.15, anchor='sw')
+T2.place(x=1280, y=100)
 
 Enter1 = Button(window, text="Enter", height=3, width=8, command=check1())
-Enter1.place(relx=0.423, rely=0.15, anchor='sw')
+Enter1.place(x=725, y=100)
 Enter2 = Button(window, text="Enter", height=3, width=8)
-Enter2.place(relx=0.953, rely=0.15, anchor='sw')
+Enter2.place(x=1630, y=100)
 Exit = Button(window, text="Exit Program", height=5, width=12, command=window.destroy)
-Exit.place(relx=0.94, rely=0.995, anchor='sw')
+Exit.place(x=1595, y=950)
 
 window.mainloop()
-
-def check1():
-    
